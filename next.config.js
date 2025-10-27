4b3584f1b7c5dca02ebfb388ebe7b41b36fa4a7a
houssein-dev <houceinhmd@gmail.com>
2025-10-27 13:16:08 +0100

    conf

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // يحول Next.js إلى static site
-  images: {
-    unoptimized: true, // ضروري للـ <Image /> 
-  },   
-  basePath: "/projet-site-Agence-Num-rique-de-l-tat",  // مهم إذا تستخدم GitHub Pages
-  assetPrefix: "/projet-site-Agence-Num-rique-de-l-tat",
-};
+  output: "export",
+  images: { unoptimized: true },
 
+basePath: "/projet-site-Agence-Num-rique-de-l-tat-main3",
+assetPrefix: "/projet-site-Agence-Num-rique-de-l-tat-main3",
+  trailingSlash: true
+};
 module.exports = nextConfig;
--- next.config.ts
+++
@@ -1,6 +0,0 @@
-// next.config.js
-const nextConfig = {
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true }, // ⚠️ temporaire seulement
-};
-module.exports = nextConfig;
